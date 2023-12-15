4b8d74def22be6ea96913d9f615e959dbb1a561a
neshwa.parveen <neshwa.parveen@chillarcards.com>
2023-12-15 11:46:39 +0530

    Initial Commit

---
+++ SwiftUI Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI Animation
+//
+//  Created by Neshwa on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
